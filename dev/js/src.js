4ee10a0e6c46077f5cee942535e62f6c41dc968e
Koiadjun <koliadjun@gmail.com>
2018-06-14 07:34:27 +0300

    slider

--- dev/js/src.js
+++ dev/js/src.js
@@ -19,3 +19,61 @@ $('[data-tabtrigger]').click(function(e){
     setParentHeight( $('[data-tab="'+n+'"][data-group="'+group+'"]'))
   }
   })
+var paragtaphs = document.getElementsByTagName('p');
+console.log(paragtaphs);
+for (var i = 0; i < paragtaphs.length; i++) {
+  paragtaphs[i].style.color=randomColor();
+  paragtaphs[i].style.background =randomColor();
+}
+function randomColor() {
+  var color = '#';
+  var colorDigit = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+  for (var i = 0; i < 6; i++) {
+    var rand = Math.floor(Math.random() * colorDigit.length);
+    color +=  colorDigit[rand];
+  }
+  return color;
+}
+/*$(paragtaphs).click(function(){
+  this.style.color = randomColor();
+  this.style.background = randomColor();
+})*/
+/*$(paragtaphs).click(function(){
+  this.style.color = randomColor();
+  this.style.background = randomColor();
+})*/
+/*var iFirst = 0;
+console.log(iFirst == 0)
+if (iFirst == 0){
+  $(paragtaphs).click(function(){
+    var colorF = this.style.color 
+    var backgroundF = this.style.background 
+    iFirst = $(this).index();
+    console.log(colorF,backgroundF,iFirst)
+  })
+console.log(!iFirst == 0)
+else {
+  $(paragtaphs).click(function(){
+    var colorS = this.style.color 
+    var backgroundS = this.style.background 
+    var iSecond= $(this).index();
+    console.log(colorS,backgroundS,iSecond)
+    this.style.color = colorF;
+    this.style.background = backgroundF;
+    paragtaphs[iFirst].style.color = colorS;
+    paragtaphs[iFirst].style.background = backgroundS;
+    iFirst=0;})
+}
+      */
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed:3000,
+    arrows:false,
+    appendDots:$('.slider__dots'),
+    dots:true,
+    lidesToShow: 1,
+    adaptiveHeight: true,
+    infinite: true,
+  });
+});
